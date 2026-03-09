c64b40e822b19defc486a653e3671e51b6c031de
unknown <gonzalessamanthagrace07@gmail.com>
2026-03-09 22:53:02 +0800

    second and third program

---
+++ Prog03.py
@@ -0,0 +1,5 @@
+ # Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+print(num1 + num2)
