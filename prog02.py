df380e932a3a2d63984ac51f337cea3fb6588987
unknown <gonzalessamanthagrace07@gmail.com>
2026-03-09 23:16:26 +0800

    I change name

---
+++ prog02.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+if num1 == num2:
+    print("Equal")
+else:
+    print("Not Equal")
