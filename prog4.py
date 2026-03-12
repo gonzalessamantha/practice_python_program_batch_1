9b65ae74db2b0cae49ea9375609e6873e8c6b0d5
unknown <gonzalessamanthagrace07@gmail.com>
2026-03-12 16:12:13 +0800

    I added up to prog 10

---
+++ prog4.py
@@ -0,0 +1,4 @@
+# Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(num1 * num2)
